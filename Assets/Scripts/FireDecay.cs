c898afcac8684485521298300f16868b40f47c2f
dj220679 <dj220679@falmouth.ac.uk>
2020-02-10 14:18:36 +0000

    Created basic ai that goes to set points and spawns fire at random points underneath it
    Signed-off-by: dj220679 <dj220679@falmouth.ac.uk>

---
+++ Assets/Scripts/FireDecay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireDecay : MonoBehaviour
+{
+	public float decayTime = 10;
+
+	float endTime = 0;
+
+
+	void Start()
+	{
+		endTime = Time.time + decayTime;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Time.time >= endTime)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/MoveAI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAI : MonoBehaviour
+{
+	public float speed = 3;
+	public Transform[] waypoints;
+
+	private int index = 0;
+	private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		MoveActor();
+
+    }
+
+
+	void MoveActor()
+	{
+		if (time < 1)
+		{
+			if (index == waypoints.Length - 1)
+				transform.position = Vector3.Lerp(waypoints[index].position, waypoints[0].position, time);
+			else
+				transform.position = Vector3.Lerp(waypoints[index].position, waypoints[index + 1].position, time);
+
+			time += 0.01f * speed;
+
+		}
+		else
+		{
+			time = 0;
+			if (index < waypoints.Length - 1)
+			{
+				index++;
+			}
+			else
+			{
+				index = 0;
+
+			}
+		}
+	}
+}
