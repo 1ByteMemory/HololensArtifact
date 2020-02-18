c9a7251ae9c2c56c7ec22f1654760c85a9f34d6e
dj220679 <dj220679@falmouth.ac.uk>
2020-02-18 18:40:21 +0000

    Added MRTK foundation unity package.
    (Text Mesh Pro Essentials was also recommended to add, so I added that)
    
    Signed-off-by: dj220679 <dj220679@falmouth.ac.uk>

--- Assets/Scripts/MoveAI.cs
+++ Assets/Scripts/MoveAI.cs
@@ -10,18 +10,51 @@ public class MoveAI : MonoBehaviour
 	private int index = 0;
 	private float time;
 
+	private Rigidbody rb;
+	public float gravitySize = 9.8f;
+
     // Start is called before the first frame update
     void Start()
     {
+		rb = GetComponent<Rigidbody>();
 		time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-		MoveActor();
+		//MoveActor();
+
+		CheckDirection();
+
 
     }
+
+
+	void CheckDirection()
+	{
+		if (Input.GetKeyDown(KeyCode.W))
+			ChangeGravity(Vector3.forward);
+
+		if (Input.GetKeyDown(KeyCode.S))
+			ChangeGravity(Vector3.back);
+
+		if (Input.GetKeyDown(KeyCode.A))
+			ChangeGravity(Vector3.left);
+
+		if (Input.GetKeyDown(KeyCode.D))
+			ChangeGravity(Vector3.right);
+
+	}
+
+
+
+	void ChangeGravity(Vector3 gravityDirection)
+	{
+		Debug.Log("Changing gravity to " + gravityDirection * gravitySize);
+		Physics.gravity = gravityDirection * gravitySize;
+	}
+
 
 
 	void MoveActor()
