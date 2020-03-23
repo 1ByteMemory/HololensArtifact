4dba15820910dbdbd9a65720e7ff51d5b95d0e4d
dj220679 <dj220679@falmouth.ac.uk>
2020-03-23 17:30:01 +0000

    Created a scene to demonstrate the fire ball

---
+++ Assets/DemoModel/CharacterMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    MagicSpell spell;
+
+    public float moveSpeed = 5;
+    public float mouseSensitivity = 10;
+
+    public GameObject head;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        spell = GetComponent<MagicSpell>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        Look();
+
+        Fire();
+    }
+
+
+    void Move()
+    {
+        
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            rb.AddForce(transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+
+            rb.AddForce(transform.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+
+    void Look()
+    {
+        Vector3 mouse = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * mouseSensitivity;
+
+        transform.localEulerAngles += mouse;
+        
+    }
+
+    void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            spell.CastFireball();
+        }
+    }
+
+}
