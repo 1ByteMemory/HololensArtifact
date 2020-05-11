9d11775315d8b9552c058841cc71ea140f090861
Dan <dj220679@falmouth.ac.uk>
2020-05-11 13:23:25 +0100

    Created Demo scene for vidoe

---
+++ Assets/DemoModel/InputSimulation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputSimulation : MonoBehaviour
+{
+    public Image selectImage;
+
+    public Sprite[] sprites; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectImage.sprite = sprites[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            selectImage.sprite = sprites[1];
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            selectImage.sprite = sprites[0];
+        }
+    }
+}
